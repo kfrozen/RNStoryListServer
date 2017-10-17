7be390e9d6fde45db427f3d46a0fa8ba59399a10
kfrozen <troythm@gmail.com>
2017-10-18 00:44:30 +0800

    Applied stream when reading files

--- routes/articles.js
+++ routes/articles.js
@@ -5,13 +5,34 @@ var readFileAsync = require('../promiseUtils');
 /* GET articles listing. */
 router.get('/', function(req, res, next) {
 
-    readFileAsync("./modals/articles.json", "utf8")
-        .then(function (data) {
-            res.status(200).send(data);
-        })
-        .catch(function (err) {
-            res.status(404).send(err.message);
+    // readFileAsync("./modals/articles.json", 'utf-8')
+    //     .then(function (data) {
+    //         res.status(200).send(data);
+    //     })
+    //     .catch(function (err) {
+    //         res.status(404).send(err.message);
+    //     });
+
+    var reader = require('fs').createReadStream("./modals/articles.json", {
+            flags: 'r',
+            encoding: 'utf8',
+            autoClose: true,
+            mode: 0666
         });
+
+    var data = "";
+
+    reader.on('data', function (chunk) {
+        data += chunk;
+    });
+
+    reader.on('end', function () {
+        res.status(200).send(data);
+    });
+
+    reader.on('error', function (err) {
+        res.status(404).send(err.message);
+    });
 });
 
 module.exports = router;
